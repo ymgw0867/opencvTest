56bc1d9cb8412739557da06078a89a6c7062a8b3
kyama <kyama@YMGW-PC>
2018-09-13 10:42:19 +0900

    プロジェクト ファイルを追加します。

---
+++ opencvTest/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace opencvTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "画像表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 542);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "拡大表示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(208, 542);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "縮小表示";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(306, 542);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 27);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 573);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 27);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "マルチTIFFの分解";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(133, 573);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(306, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(428, 514);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 514);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 615);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ opencvTest/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OpenCvSharp;
+
+namespace opencvTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const float B_WIDTH = 0.25f;
+        const float B_HEIGHT = 0.25f;
+
+        const float A_WIDTH = 0.05f;
+        const float A_HEIGHT = 0.05f;
+
+        float n_width = 0f;
+        float n_height = 0f;
+
+        Mat mMat = new Mat();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n_width = B_WIDTH;
+            n_height = B_HEIGHT;
+
+            imgShow(@"C:\CONYX_OCR\TIF\201808\201808_87654323_山田　哲人.tif", n_width, n_height);
+
+            trackBar1.Value = 0;
+        }
+
+        private void imgShow(string filePath, float w, float h)
+        {
+            mMat = new Mat(filePath, ImreadModes.GrayScale);
+            Bitmap bt = MatToBitmap(mMat);
+
+            // Bitmap を生成
+            Bitmap canvas = new Bitmap((int)(bt.Width * w), (int)(bt.Height * h));
+
+            Graphics g = Graphics.FromImage(canvas);
+
+            g.DrawImage(bt, 0, 0, bt.Width * w, bt.Height * h);
+
+            //メモリクリア
+            bt.Dispose();
+            g.Dispose();
+
+            pictureBox1.Image = canvas;
+        }
+        private void imgShow(Mat mImg, float w, float h)
+        {
+            int cWidth = 0;
+            int cHeight = 0;
+
+            Bitmap bt = MatToBitmap(mImg);
+
+            // Bitmapサイズ
+            if (panel1.Width < (bt.Width * w) || panel1.Height < (bt.Height * h))
+            {
+                cWidth = (int)(bt.Width * w);
+                cHeight = (int)(bt.Height * h);
+            }
+            else
+            {
+                cWidth = panel1.Width;
+                cHeight = panel1.Height;
+            }
+
+            // Bitmap を生成
+            Bitmap canvas = new Bitmap(cWidth, cHeight);
+
+            // ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            // 画像をcanvasの座標(0, 0)の位置に指定のサイズで描画する
+            g.DrawImage(bt, 0, 0, bt.Width * w, bt.Height * h);
+
+            //メモリクリア
+            bt.Dispose();
+            g.Dispose();
+
+            // PictureBox1に表示する
+            pictureBox1.Image = canvas;
+        }
+
+        // GUI上に画像を表示するには、OpenCV上で扱うMat形式をBitmap形式に変換する必要がある
+        public static Bitmap MatToBitmap(Mat image)
+        {
+            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            n_width += A_WIDTH;
+            n_height += A_HEIGHT;
+
+            //imgShow(@"C:\CONYX_OCR\TIF\201808\201808_87654323_山田　哲人.tif", n_width, n_height);
+            imgShow(mMat, n_width, n_height);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (n_width > A_WIDTH)
+            {
+                n_width -= A_WIDTH;
+            }
+
+            if (n_height > A_HEIGHT)
+            {
+                n_height -= A_HEIGHT;
+            }
+
+            //imgShow(@"C:\CONYX_OCR\TIF\201808\201808_87654323_山田　哲人.tif", n_width, n_height);
+            imgShow(mMat, n_width, n_height);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Mat rMat = imgRotate(new Mat(@"C:\CONYX_OCR\TIF\201808\201808_87654323_山田　哲人.tif", ImreadModes.GrayScale), true);
+
+            //imgShow(rMat, n_width, n_height);
+
+            imgRotateBmp(pictureBox1.Image);
+
+            //mMat = imgRotate(mMat, true);
+            //imgShow(mMat, n_width, n_height);
+        }
+
+        private Mat imgRotate(Mat img, bool isRight)
+        {
+            Mat dest = new Mat();
+
+            Point2f center = new Point2f(img.Cols / 2, img.Rows / 2);
+            //Point2f center = new Point2f(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            var rMat = Cv2.GetRotationMatrix2D(center, 90, 1);
+            Cv2.WarpAffine(img, dest, rMat, img.Size());
+
+            Cv2.ImShow("img", dest);
+            Cv2.WaitKey();
+
+
+
+            //Cv2.Flip(img, dest, FlipMode.XY);
+
+
+
+
+            return dest;
+        }
+
+        private void imgRotateBmp(Image img)
+        {
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = img;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            showMultiTiff(@"C:\CONYX_OCR\CCF_000023.tif");
+        }
+
+        private void showMultiTiff(string tiffFileName)
+        {
+            FileStream tifFS = new FileStream(tiffFileName, FileMode.Open, FileAccess.Read);
+            Image gim = Image.FromStream(tifFS);
+            FrameDimension gfd = new FrameDimension(gim.FrameDimensionsList[0]);
+            int pageCount = gim.GetFrameCount(gfd);//全体のページ数を得る 
+
+            //System.Diagnostics.Debug.WriteLine(pageCount);
+            //Graphics g = pictureBox1.CreateGraphics();
+
+            for (int i = 0; i < pageCount; i++)
+            {
+                gim.SelectActiveFrame(gfd, i);
+
+                gim.Save(@"C:\multitif\0902" + i.ToString().PadLeft(3, '0') + ".tif", ImageFormat.Tiff);
+
+                //g.DrawImage(gim, 0, 0, pictureBox1.Width, pictureBox1.Height);//PictureBoxに表示してます
+                //System.Threading.Thread.Sleep(500);
+            }
+
+            MessageBox.Show("finish!!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 50;
+            trackBar1.Value = 0;
+
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 10;
+
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {            
+            n_width = B_WIDTH + (float)trackBar1.Value * 0.05f;
+            n_height = B_HEIGHT + (float)trackBar1.Value * 0.05f;
+
+            //imgShow(@"C:\CONYX_OCR\TIF\201808\201808_87654323_山田　哲人.tif", n_width, n_height);
+            imgShow(mMat, n_width, n_height);
+        }
+    }
+}
